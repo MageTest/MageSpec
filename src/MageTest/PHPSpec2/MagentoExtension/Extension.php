f57ae37a2b5301c137298e2e5ee8838f64fa4861
Marcello Duarte <marcello.duarte@gmail.com>
2012-11-12 14:20:17 +0000

    Amend extension to inject loader into locator

--- src/MageTest/PHPSpec2/MagentoExtension/Extension.php
+++ src/MageTest/PHPSpec2/MagentoExtension/Extension.php
@@ -4,9 +4,10 @@ namespace MageTest\PHPSpec2\MagentoExtension;
 
 use PHPSpec2\Extension\ExtensionInterface,
     PHPSpec2\Console\ExtendableApplicationInterface as ApplicationInterface,
-    PHPSpec2\Configuration\Configuration;
+    PHPSpec2\Configuration\Configuration,
+    PHPSpec2\Runner\Locator;
 
-use MageTest\PHPSpec2\MagentoExtension\Loader\SpecificationClassLoader;
+use MageTest\PHPSpec2\MagentoExtension\Loader\SpecificationsClassLoader;
 use PHPSpec2\ServiceContainer;
 
 class Extension implements ExtensionInterface
@@ -16,7 +17,11 @@ class Extension implements ExtensionInterface
     public function initialize(ServiceContainer $container)
     {
         $container->set('specifications_loader', $container->share(function($c) {
-            return new SpecificationClassLoader;
+            return new SpecificationsClassLoader;
+        }));
+
+        $container->set('locator', $container->share(function($c) {
+            return new Locator($c('specifications_loader'));
         }));
     }
 }
