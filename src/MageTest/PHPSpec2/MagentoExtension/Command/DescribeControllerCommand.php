d5d5db9abef7e97b2cb8dc153b496a30a8d2b8e0
Alistair Stead <alistair.stead@designdisclosure.com>
2012-11-13 15:52:02 +0000

    WIP for package commands

--- src/MageTest/PHPSpec2/MagentoExtension/Command/DescribeControllerCommand.php
+++ src/MageTest/PHPSpec2/MagentoExtension/Command/DescribeControllerCommand.php
@@ -9,6 +9,13 @@ class DescribeControllerCommand extends DescribeCommand
     public function __construct()
     {
         parent::__construct();
+
+        $this->setDefinition(array(
+            new InputArgument('spec', InputArgument::REQUIRED, 'Spec to describe'),
+            new InputOption('src-path', null, InputOption::VALUE_REQUIRED, 'Source path', 'app/code/local'),
+            new InputOption('spec-path', null, InputOption::VALUE_REQUIRED, 'Specs path', 'spec/app/code/local'),
+            new InputOption('namespace', null, InputOption::VALUE_REQUIRED, 'Specs NS', 'spec\\'),
+        ));
     }
 
     /**
@@ -27,4 +34,12 @@ EOF
         ;
     }
 
+
+
+    protected function getSpecContentFor(array $parameters)
+    {
+        $template = file_get_contents(__DIR__.'/../Resources/templates/controller-spec.php');
+
+        return strtr($template, $parameters);
+    }
 }
