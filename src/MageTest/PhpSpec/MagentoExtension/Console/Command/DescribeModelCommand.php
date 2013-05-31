1716e60094f1c4956344998df4aad5cdbca8fedc
marcodebortoli <marco.debo.debortoli@gmail.com>
2013-05-31 19:18:14 +0100

    As a MageSpec user I need to be guided in the use of the right model naming convention to adhere to the Magento standards

--- src/MageTest/PhpSpec/MagentoExtension/Console/Command/DescribeModelCommand.php
+++ src/MageTest/PhpSpec/MagentoExtension/Console/Command/DescribeModelCommand.php
@@ -21,6 +21,8 @@
  */
 namespace MageTest\PhpSpec\MagentoExtension\Console\Command;
 
+use PhpSpec\Exception\Example\ErrorException;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
@@ -37,21 +39,37 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class DescribeModelCommand extends Command
 {
+    const VALIDATOR = '/^([a-z0-9]+)_([a-z0-9]+)\/([a-z0-9]+)(_[\w]+)?$/';
+
     public function __construct()
     {
         parent::__construct('describe:model');
 
         $this->setDefinition(array(
-            new InputArgument('class', InputArgument::REQUIRED, 'Class to describe'),
+            new InputArgument('model_alias', InputArgument::REQUIRED, 'Magento Model alias to be described'),
         ));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $model = $input->getArgument('model_alias');
+
+        if ((bool) preg_match(self::VALIDATOR, $model) === false) {
+            $message = <<<ERR
+The model alias provided doesn't follow the Magento naming conventions.
+Please make sure it looks like the following:
+
+  vendorname_modulename/modelname
+
+Please pay attention to words case
+ERR;
+            throw new \InvalidArgumentException($message);
+        }
+
         $container = $this->getApplication()->getContainer();
         $container->configure();
 
-        $classname = 'model:' . $input->getArgument('class');
+        $classname = 'model:' . $model;
         $resource  = $container->get('locator.resource_manager')->createResource($classname);
 
         $container->get('code_generator')->generate($resource, 'specification');
