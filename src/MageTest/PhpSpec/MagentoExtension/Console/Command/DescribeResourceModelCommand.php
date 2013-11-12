24a371c13155b453ba4336a31d53d91110cab074
Arturas Smorgun <arturas@smorgun.lt>
2013-11-12 18:17:14 +0000

    Suggest using lower case to describe magento objects.
    We can allow uppercase for edge cases with legacy code, but the convention
    is to use lowercase and magespec should suggest to use lowercase.

--- src/MageTest/PhpSpec/MagentoExtension/Console/Command/DescribeResourceModelCommand.php
+++ src/MageTest/PhpSpec/MagentoExtension/Console/Command/DescribeResourceModelCommand.php
@@ -56,7 +56,7 @@ class DescribeResourceModelCommand extends Command
 The resource model alias provided doesn't follow the Magento naming conventions.
 Please make sure it looks like the following:
 
-  Vendorname_Modulename/Blockname
+  vendorname_modulename/resourcemodelname
 
 The lowercase convention is used because it reflects the best practice
 convention within the Magento community. This reflects the identifier that
