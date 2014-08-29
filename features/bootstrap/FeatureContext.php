fb0f6aea44c08b36f4c2204f1fb78b8950ba238d
Shane Auckland <shane.auckland@gmail.com>
2014-08-29 19:48:10 +0100

    Add initial behat setup

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,21 @@
+<?php
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Behat context class.
+ */
+class FeatureContext implements SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets it's own context object.
+     * You can also pass arbitrary arguments to the context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
